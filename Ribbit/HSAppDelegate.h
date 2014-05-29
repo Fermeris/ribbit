0b8f1fc79b78fedca60e0b45bcd411d70225565a
User - HS <User1@Harijss-MacBook-Pro.local>
2014-05-29 18:06:49 +0300

    Initial Commit

---
+++ Ribbit/HSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HSAppDelegate.h
+//  Ribbit
+//
+//  Created by User - HS on 29/05/14.
+//  Copyright (c) 2014 Ferma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by User - HS on 29/05/14.
+//  Copyright (c) 2014 Ferma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HSAppDelegate class]));
+    }
+}
